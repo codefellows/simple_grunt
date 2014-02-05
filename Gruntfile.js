458812a89eb03ac8c45b8109ee7a2a7249c54b25
Cooper <cooper.trowbridge@gmail.com>
2014-02-05 09:02:07 +0000

    add jshint,etc to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,59 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    simplemocha: {
-      options: {
-        timeout: 3000,
-        ignoreLeaks: false,
-        reporter: 'tap'
-      },
-
-      all: { src: ['test/**/*.js'] }
-    }
+      pkg: grunt.file.readJSON('package.json'),
+      concat: {
+          options: {
+              separator: ';'
+          },
+          dist: {
+              src: ['src/**/*.js'],
+              dest: 'dist/<%= pkg.name %>.js'
+          }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+        qunit: {
+            files: ['test/**/*.html']
+        },
+        jshint: {
+            files: ['test/**/*.html']
+        },
+        jshint: {
+            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+            options: {
+                //options here to override JSHint defaults
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true,
+                    document: true
+                }
+            }
+        },
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint', 'quint']
+        }
   });
 
   // For this to work, you need to have run `npm install grunt-simple-mocha`
   grunt.loadNpmTasks('grunt-simple-mocha');
-
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   // Add a default task. This is optional, of course :)
-  grunt.registerTask('default', 'simplemocha');
+  grunt.registerTask('default', ['simplemocha', 'jshint', 'qunit', 'concat', 'uglify']);
+  grunt.registerTask('test', ['jshint', 'qunit']);
 
 };
